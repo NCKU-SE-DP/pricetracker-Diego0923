353b637998eaedcec1a25a759f54c6493d3a3c0c
Diego0923 <diego288205@gmail.com>
2024-12-01 01:22:13 +0800

    Update schemas.py
    fix file error

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -1,20 +1,51 @@
-from fastapi import APIRouter, Query
-import requests
-
-router = APIRouter()
-
-@router.get("/api/v1/prices/necessities-price")
-async def get_necessities_prices(
-    category: str = Query(None), 
-    commodity: str = Query(None)
-):
-    """取得民生物價資料"""
-    return requests.get(
-        "https://opendata.ey.gov.tw/api/ConsumerProtection/NecessitiesPrice",
-        params={"CategoryName": category, "Name": commodity},
-    ).json() 
+from pydantic import BaseModel
+from typing import Optional
+class UserAuthSchema(BaseModel):
     """
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Failed to fetch necessities prices")
-    return response.json()
+    用於用戶註冊和登入的數據模式
     """
+    username: str
+    password: str
+    password: Optional[str] = None
+
+class UserProf(BaseModel):
+    id: int
+    username: str
+    class Config:
+        orm_mode = True
+
+class PromptRequest(BaseModel):
+    """
+    用於新聞搜尋的關鍵字請求模式
+    """
+    prompt: str
+
+
+class NewsSummaryRequestSchema(BaseModel):
+    """
+    用於新聞摘要生成的數據模式
+    """
+    content: str
+
+
+class NewsArticleSchema(BaseModel):
+    """
+    用於回應的新聞文章數據模式
+    """
+    id: int
+    url: str
+    title: str
+    time: str
+    content: str
+    summary: str
+    reason: str
+
+    class Config:
+        orm_mode = True
+
+
+class NewsUpvoteResponseSchema(BaseModel):
+    """
+    用於回應的新聞文章點贊數據模式
+    """
+    message: str
