a346404cf73bb414afb1801227b3c043668e9355
Diego <Diego288205@gmail.com>
2024-12-19 02:12:36 +0800

    adding handler to database.py

--- backend/src/database.py
+++ backend/src/database.py
@@ -3,12 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import DATABASE_URL
-
+import logging
 # 創建資料庫引擎
 engine = create_engine(DATABASE_URL, echo=True)
 
 # 創建會話生成器
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 設定 Logger
+logger = logging.getLogger(__name__)
 
 # 定義基礎模型類
 Base = declarative_base()
@@ -21,5 +24,8 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception as e:
+        logger.error(f"Error occurred in database session: {e}")
+        raise
     finally:
         db.close()
